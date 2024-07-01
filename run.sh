68ff18ae64b07ddb68721c08e8dd9eaa1ceab979
wushuo <2725730722@qq.com>
2024-07-01 09:30:36 +0800

    init

--- run.sh
+++ run.sh
@@ -10,6 +10,8 @@ memberId=$(sudo zerotier-cli info | awk -F" " '{print $3}')
 echo "Member Id is ${memberId}"
 curl -X POST -H "Content-Type: application/json" -H "Authorization: token ${ZEROTIER_TOKEN}" -d "{\"config\": { \"ipAssignments\": [ \"$IP_ASSIGNMENT\" ] }}" https://api.zerotier.com/api/v1/network/${ZEROTIER_NETWORK_ID}/member/${memberId}
 
+echo ${ZEROTIER_NETWORK_ID}
+
 sudo zerotier-cli join ${ZEROTIER_NETWORK_ID}
 
 sleep 10
